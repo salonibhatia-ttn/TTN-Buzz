5385163fcc1ce9a0e5c999ba55c0fcfb91a7e305
sandeepbttn <sandeep.bhatt1@tothenew.com>
2022-04-05 17:19:57 +0530

    connecting to nodejs

--- ttn_buzz/src/App.js
+++ ttn_buzz/src/App.js
@@ -9,14 +9,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+
+        <form action="../../post" method="post" className="form">
+            <button type="submit">Connected</button>
+        </form>
       </header>
     </div>
   );
