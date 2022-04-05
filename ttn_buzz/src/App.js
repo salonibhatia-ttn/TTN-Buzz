026521ba2cd2f56d265517773c16ff612b447834
sandeepbttn <sandeep.bhatt1@tothenew.com>
2022-04-05 20:34:15 +0530

    adding dotenv package

--- ttn_buzz/src/App.js
+++ ttn_buzz/src/App.js
@@ -7,10 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         A simple React app...
         </p>
-        
-
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
         <form action="../../post" method="post" className="form">
             <button type="submit">Connected</button>
         </form>
