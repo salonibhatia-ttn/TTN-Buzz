da3b1909213d1c7789d7a9796d943f700a5eb25d
salonibhatia-ttn <saloni.bhatia@tothenew.com>
2022-04-05 15:16:18 +0530

    file structure setup

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+//Listening on port 3000
+app.listen(3000, () => {
+  console.log('Listening on port 3000...');
+});
