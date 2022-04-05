0af6cc45bf5cbf831031d309b782a2e2e4266b36
sandeepbttn <sandeep.bhatt1@tothenew.com>
2022-04-05 17:18:03 +0530

    connecting to react

--- server/index.js
+++ server/index.js
@@ -2,6 +2,19 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
+
+
+app.get("/", (req,res)=>{
+  res.send("hello world from routes");
+})
+
+// connecting wtih react
+app.post("/post",(req,res)=>{
+  // res.send("hello world");
+  console.log("connected to React");
+  res.redirect("/");
+})
+
 //Listening on port 3000
 app.listen(3000, () => {
   console.log('Listening on port 3000...');
