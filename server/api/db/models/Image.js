4eaac92cdd20064040afe9534ff303de593a8caf
salonibhatia-ttn <saloni.bhatia@tothenew.com>
2022-04-05 19:04:22 +0530

    Updated Comment and Image Schemas

---
+++ server/api/db/models/Image.js
@@ -0,0 +1,21 @@
+const mongoose = require ('mongoose');
+const Post = require('./Post');
+//ImageSchema 
+const imageSchema = new mongoose.Schema({
+ postId : {
+   type: String,
+   ref: 'Post',
+   required: true
+ },
+ url : {
+   type: String,
+   required: true
+ },
+ dateCreated : {
+   type: Date, 
+   required:true
+ }
+});
+
+const Image = mongoose.model('IMAGE', imageSchema);
+module.exports = Image;
