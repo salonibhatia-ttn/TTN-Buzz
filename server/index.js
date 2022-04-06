c9cbd9f044a8699a7d25559522040ec8756dcaf7
sandeepbttn <sandeep.bhatt1@tothenew.com>
2022-04-06 18:20:08 +0530

    connectiing to mongoose

--- server/index.js
+++ server/index.js
@@ -1,9 +1,29 @@
-const mongoose = require('mongoose');
+// 
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+// connecting database using mongoose
+
+const DB = "mongodb+srv://sandeep02bhatt:tothenew@cluster0.uf9e0.mongodb.net/TTN_BUZZ?retryWrites=true&w=majority";
 
 
-//Listening on port 3000
-app.listen(3000, () => {
-  console.log('Listening on port 3000...');
-});
+mongoose.connect(DB).then(()=>{
+    console.log(`connection successful`);
+}).catch((err)=>console.log(`no connection`));
+
+app.use(express.json());
+
+
+// assigning port  number
+const PORT = process.env.PORT || 3001;
+
+
+app.get("/", (req,res)=>{
+    res.send("hello world");
+})
+
+
+
+app.listen(PORT, console.log(`Server on port ${PORT}`));
+
