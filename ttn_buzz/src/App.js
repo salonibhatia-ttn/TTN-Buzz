08240615f747c5fe48f175f510e65be98c466313
sandeepbttn <sandeep.bhatt1@tothenew.com>
2022-04-06 13:24:37 +0530

    updating files

--- ttn_buzz/src/App.js
+++ ttn_buzz/src/App.js
@@ -1,28 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './components/Navbar'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         A simple React app...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <form action="../../post" method="post" className="form">
-            <button type="submit">Connected</button>
-        </form>
-      </header>
-    </div>
-  );
-}
+  <div>
+   <Navbar/> 
+   hello world
 
-export default App;
+  {/* <form action="../../post" method="post" className="form">   
+      <button type="submit">Connected</button>
+  </form> */}
+   
+  </div>
+  )
+}
